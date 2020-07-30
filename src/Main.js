233dfc3a78ea8ba0108f483a712b8ae5f6e09bc0
Yibin Long <yibinlong@outlook.com>
2020-07-30 04:40:57 -0400

    switch to functional components, switch to scss

---
+++ src/Main.js
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+import {
+  Route,
+  NavLink,
+  HashRouter
+} from "react-router-dom";
+import Home from "./Home";
+import AboutUs from "./AboutUs";
+import Members from "./Members";
+import Campaigns from "./Campaigns";
+ 
+const Main = (props) => {
+    return (
+        <HashRouter>
+            <div>
+                <h1>PRISE Profile</h1>
+                <ul className="header">
+                    <li><NavLink exact to="/">Home</NavLink></li>
+                    <li><NavLink to="/AboutUs">About Us</NavLink></li>
+                    <li><NavLink to="/Members">Members</NavLink></li>
+                    <li><NavLink to="/Campaigns">Campaigns</NavLink></li>
+                </ul>
+                <div className="content">
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/AboutUs" component={AboutUs}/>
+                    <Route path="/Members" component={Members}/>
+                    <Route path="/Campaigns" component={Campaigns}/>
+                </div>
+            </div>
+        </HashRouter>
+    );
+  
+}
+ 
+export default Main;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./components/App";
-
-ReactDOM.render(<App />, document.getElementById("root"));
+import Main from "./Main";
+import "./index.scss";
+ 
+ReactDOM.render(
+  <Main/>, 
+  document.getElementById("root")
+);
