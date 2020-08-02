0da983ab299ebbfff1734210faa0b8d7fc01fbd6
ShionFujimori <shion888@gmail.com>
2020-08-03 00:20:36 +0900

    Add basic profile components

---
+++ src/pages/ProfilePage/ProfileSubPage/AboutUs.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+const AboutUs = (props) => {
+    return (
+      <div>
+        <h2>About Us</h2>
+        <p>Here is information about our company/student organization!</p>
+        <ol>
+          <li>Our Mission</li>
+          <li>Goals</li>
+          <li>Events running this year</li>
+          <li>How to help</li>
+          <li>What we do</li>
+        </ol>
+      </div>
+    );
+}
+
+export default AboutUs;
