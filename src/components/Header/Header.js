c1f277bb1b79f310b6de366954d860f4fc1af793
1999akinori <1999akinori@gmail.com>
2020-09-03 18:18:39 -0400

    made the homePage more responsive and made the changes fro figma

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,6 +3,7 @@ import styles from "./Header.module.scss";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Navbar from "../Navbar/Navbar";
+import Button from "@material-ui/core/Button";
 
 const Header = () => {
   return (
@@ -12,6 +13,10 @@ const Header = () => {
       </Grid>
       <Grid item>
         <Navbar />
+      </Grid>
+      <Grid item>
+        <Button className={styles.buttonLogIn}>Log in</Button>
+        <Button className={styles.buttonSignUp} color="primary" disableElevation>Sign Up</Button>
       </Grid>
     </Grid>
   );
